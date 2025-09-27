4c5aeae85aef07d2bbd7dfaebfc749decbc264b9
khaigyr <khaiigyr@gmail.com>
2025-09-27 23:16:08 +1000

    added methods exam countdown

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -186,52 +186,62 @@
 	</form>
 {/if}
 
-<div class="flex flex-col gap-4">
-    <p class="font-bold text-xl">Previous study session</p>
-    <div class="overflow-x-auto">
-        <div class="min-w-max">
-            <div class="grid grid-cols-7 border-b border-neutral-200">
-                <p class="col-span-2 px-4 py-2">Date</p>
-                <p class="col-span-2 px-4 py-2">Subject</p>
-                <p class="px-4 py-2">Duration</p>
-                <p class="px-4 py-2">Rating</p>
-                <p class="px-4 py-2 text-center">{' '}</p>
-            </div>
-
-            {#each sessions as s}
-                <div
-                    class="grid grid-cols-7 items-center divide-x divide-neutral-200 border-b border-neutral-200"
-                >
-                    <p class="col-span-2 px-4 py-2">{s.data.date}</p>
-                    <p class="col-span-2 px-4 py-2">{s.data.subject}</p>
-                    <p class="px-4 py-2">{Math.floor(s.data.duration / 1000 / 60) % 60} mins</p>
-                    <p class="px-4 py-2">{s.data.rating}/20</p>
-                    <div class="px-4 py-2 text-center">
-                        <form action="?/deleteSession" method="post">
-                            <input type="hidden" name="id" value={s.key} />
-                            <button
-                                aria-label="delete"
-                                class="cursor-pointer text-red-500 hover:underline"
-                                title="Delete session"
-                            >
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 16 16"
-                                    fill="currentColor"
-                                    class="size-4"
-                                >
-                                    <path
-                                        fill-rule="evenodd"
-                                        d="M5 3.25V4H2.75a.75.75 0 0 0 0 1.5h.3l.815 8.15A1.5 1.5 0 0 0 5.357 15h5.285a1.5 1.5 0 0 0 1.493-1.35l.815-8.15h.3a.75.75 0 0 0 0-1.5H11v-.75A2.25 2.25 0 0 0 8.75 1h-1.5A2.25 2.25 0 0 0 5 3.25Zm2.25-.75a.75.75 0 0 0-.75.75V4h3v-.75a.75.75 0 0 0-.75-.75h-1.5ZM6.05 6a.75.75 0 0 1 .787.713l.275 5.5a.75.75 0 0 1-1.498.075l-.275-5.5A.75.75 0 0 1 6.05 6Zm3.9 0a.75.75 0 0 1 .712.787l-.275 5.5a.75.75 0 0 1-1.498-.075l.275-5.5a.75.75 0 0 1 .786-.711Z"
-                                        clip-rule="evenodd"
-                                    />
-                                </svg></button
-                            >
-                        </form>
-                    </div>
-                </div>
-            {/each}
-        </div>
-    </div>
+<div>
+	<p>
+		<span class="text-neutral-400">Days till Methods Exam 1:</span>
+		<span>{Math.ceil((new Date(2025, 10, 5) - new Date()) / (1000 * 60 * 60 * 24))}</span>
+	</p>
+	<p>
+		<span class="text-neutral-400">Days till Methods Exam 2:</span>
+		<span>{Math.ceil((new Date(2025, 10, 6) - new Date()) / (1000 * 60 * 60 * 24))}</span>
+	</p>
 </div>
 
+<div class="flex flex-col gap-4">
+	<p class="text-xl font-bold">Previous study session</p>
+	<div class="overflow-x-auto">
+		<div class="min-w-max">
+			<div class="grid grid-cols-7 border-b border-neutral-200">
+				<p class="col-span-2 px-4 py-2">Date</p>
+				<p class="col-span-2 px-4 py-2">Subject</p>
+				<p class="px-4 py-2">Duration</p>
+				<p class="px-4 py-2">Rating</p>
+				<p class="px-4 py-2 text-center">{' '}</p>
+			</div>
+
+			{#each sessions as s}
+				<div
+					class="grid grid-cols-7 items-center divide-x divide-neutral-200 border-b border-neutral-200"
+				>
+					<p class="col-span-2 px-4 py-2">{s.data.date}</p>
+					<p class="col-span-2 px-4 py-2">{s.data.subject}</p>
+					<p class="px-4 py-2">{Math.floor(s.data.duration / 1000 / 60) % 60} mins</p>
+					<p class="px-4 py-2">{s.data.rating}/20</p>
+					<div class="px-4 py-2 text-center">
+						<form action="?/deleteSession" method="post">
+							<input type="hidden" name="id" value={s.key} />
+							<button
+								aria-label="delete"
+								class="cursor-pointer text-red-500 hover:underline"
+								title="Delete session"
+							>
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									viewBox="0 0 16 16"
+									fill="currentColor"
+									class="size-4"
+								>
+									<path
+										fill-rule="evenodd"
+										d="M5 3.25V4H2.75a.75.75 0 0 0 0 1.5h.3l.815 8.15A1.5 1.5 0 0 0 5.357 15h5.285a1.5 1.5 0 0 0 1.493-1.35l.815-8.15h.3a.75.75 0 0 0 0-1.5H11v-.75A2.25 2.25 0 0 0 8.75 1h-1.5A2.25 2.25 0 0 0 5 3.25Zm2.25-.75a.75.75 0 0 0-.75.75V4h3v-.75a.75.75 0 0 0-.75-.75h-1.5ZM6.05 6a.75.75 0 0 1 .787.713l.275 5.5a.75.75 0 0 1-1.498.075l-.275-5.5A.75.75 0 0 1 6.05 6Zm3.9 0a.75.75 0 0 1 .712.787l-.275 5.5a.75.75 0 0 1-1.498-.075l.275-5.5a.75.75 0 0 1 .786-.711Z"
+										clip-rule="evenodd"
+									/>
+								</svg></button
+							>
+						</form>
+					</div>
+				</div>
+			{/each}
+		</div>
+	</div>
+</div>
